6ae9a73d7fef1aa76750a0adfb5170649dc1c961
pjotrekk <pi.szuberski1@gmail.com>
2019-10-24 18:02:21 +0200

    Use Sorter the static way

--- src/sorter.h
+++ src/sorter.h
@@ -6,6 +6,7 @@
 
 class Sorter {
 public:
+  Sorter() = delete;
 
   template<
     typename Iter,
--- test/sorter_test.cpp
+++ test/sorter_test.cpp
@@ -16,13 +16,12 @@ TEST(sorter, heap_sort) {
   std::vector<int> shuffled{5, 3, 3, 4, 1, 0};
   std::vector<int> empty{};
   std::vector<int> singleton{2};
-  Sorter sorter;
 
-  sorter.heap_sort(asc_sorted, std::greater<int>());
-  sorter.heap_sort(desc_sorted, std::greater<int>());
-  sorter.heap_sort(shuffled, std::greater<int>());
-  sorter.heap_sort(empty, std::greater<int>());
-  sorter.heap_sort(singleton, std::greater<int>());
+  Sorter::heap_sort(asc_sorted, std::greater<int>());
+  Sorter::heap_sort(desc_sorted, std::greater<int>());
+  Sorter::heap_sort(shuffled, std::greater<int>());
+  Sorter::heap_sort(empty, std::greater<int>());
+  Sorter::heap_sort(singleton, std::greater<int>());
 
   std::vector<int> expected{5, 4, 3, 3, 1, 0};
 
@@ -35,9 +34,8 @@ TEST(sorter, heap_sort) {
 
 TEST(sorter, insertion_sort) {
   std::vector<int> shuffled{5, 3, 3, 4, 1, 0};
-  Sorter sorter;
 
-  sorter.insertion_sort(shuffled.begin(), shuffled.end(), std::greater<int>());
+  Sorter::insertion_sort(shuffled.begin(), shuffled.end(), std::greater<int>());
 
   std::vector<int> expected{0, 1, 3, 3, 4, 5};
 
