4bed15a4775cf36f4c76d575f1d4d0af49b07cad
pjotrekk <pi.szuberski1@gmail.com>
2019-10-24 20:52:57 +0200

    Change heap design

--- src/heap.h
+++ src/heap.h
@@ -7,23 +7,14 @@
  */
 template<typename T, typename Cmp = std::greater<T>>
 class Heap {
-public:
-  Heap(std::vector<T>& in, const Cmp& cmp = Cmp()) : v(in), comparator(cmp) {
-    heapify();
-  };
-  /* less - ascending, greater - descending */
-  template<typename Type>
-  static void HeapSort(std::vector<Type>& v, const Cmp& cmp = Cmp()) {
-    Heap<T, Cmp> heap(v, cmp);
-    for (long i = v.size() - 1; i > 0; --i) {
-      std::swap(v[0], v[i]);
-      heap.downheap(0, i);
-    }
-  }
 
 private:
   std::vector<T>& v;
   const Cmp& comparator;
+
+  Heap(std::vector<T>& in, const Cmp& cmp = Cmp()) : v(in), comparator(cmp) {
+    heapify();
+  };
 
   void heapify() {
     long begin = v.size() / 2 - 1;
@@ -71,6 +62,17 @@ private:
         break;
       }
     } while (!isLeaf(i, to));
+  }
+
+public:
+
+  /* less - descending, greater - ascending */
+  static void HeapSort(std::vector<T>& v, const Cmp& cmp = Cmp()) {
+    Heap<T, Cmp> heap(v, cmp);
+    for (long i = v.size() - 1; i > 0; --i) {
+      std::swap(v[0], v[i]);
+      heap.downheap(0, i);
+    }
   }
 
 };
